61f8d48fed703e4cf3588b086b9e91d327cb33d0
Gustavo A Martini <gutamen@live.com>
2024-12-16 23:59:28 -0300

    spring tests

---
+++ src/main/java/MyApplication.java
@@ -0,0 +1,14 @@
+@RestController
+@SpringBootApplication
+public class MyApplication {
+
+	@RequestMapping("/")
+	String home() {
+		return "Hello World!";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyApplication.class, args);
+	}
+
+}
